19b663cf4388a8f906db86a88d056bf0ea23f66d
Sven Diederichs <22592421+zaurask@users.noreply.github.com>
2021-12-04 19:39:46 +0100

    solution for day 3 part 2

--- src/bin/day3/main.rs
+++ src/bin/day3/main.rs
@@ -29,13 +29,71 @@ fn part1(input: &str) -> u32 {
 }
 
 fn part2(input: &str) -> u32 {
-    todo!()
+    let lines: Vec<_> = input.lines().map(|s| s.as_bytes()).collect();
+
+    let mut oxygen = lines.clone();
+    for pos in 0.. {
+        let mut ones = 0;
+        for num in oxygen.clone().into_iter() {
+            if num[pos] == b'1' {
+                ones += 1;
+            }
+        }
+        let keep = ones >= (oxygen.len() - ones);
+        oxygen = oxygen
+            .clone()
+            .into_iter()
+            .filter(|num| {
+                let num = num[pos];
+                if keep {
+                    num == b'1'
+                } else {
+                    num == b'0'
+                }
+            })
+            .collect();
+
+        if oxygen.len() == 1 {
+            break;
+        }
+    }
+    let oxygen_rating = u32::from_str_radix(std::str::from_utf8(oxygen[0]).unwrap(), 2).unwrap();
+
+    let mut co2 = lines;
+    for pos in 0.. {
+        let mut zeros = 0;
+        for num in co2.clone().into_iter() {
+            if num[pos] == b'0' {
+                zeros += 1;
+            }
+        }
+        let keep = zeros <= (co2.len() - zeros);
+        co2 = co2
+            .clone()
+            .into_iter()
+            .filter(|num| {
+                let num = num[pos];
+                if keep {
+                    num == b'0'
+                } else {
+                    num == b'1'
+                }
+            })
+            .collect();
+
+        if co2.len() == 1 {
+            break;
+        }
+    }
+    let co2_rating = u32::from_str_radix(std::str::from_utf8(co2[0]).unwrap(), 2).unwrap();
+
+    co2_rating * oxygen_rating
 }
 
 fn main() {
     let input = include_str!("input.txt");
     println!("{}", part1(input));
-    //println!("{}", part2(input));
+    println!("{}", part2(input));
 }
 
 #[cfg(test)]
